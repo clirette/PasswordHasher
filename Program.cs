8afe7c761720942a351eaf06c599d9476d1f70e2
Chase Lirette <chaselirette@gmail.com>
2020-03-10 20:52:07 -0500

    Added UserModel example.

--- Program.cs
+++ Program.cs
@@ -6,16 +6,35 @@ namespace console_auth
   {
     static void Main(string[] args)
     {
-      PasswordHasher<string> hasher = new PasswordHasher<string>();
+      PasswordHasher<UserModel> hasher = new PasswordHasher<UserModel>();
+      var user = new UserModel
+      {
+        FirstName = "Chase",
+        LastName = "Lirette",
+        UserName = "chaselirette"
+      };
+
       Console.WriteLine("Set your password");
-      var password = Console.ReadLine();
-      var hashedPassword = hasher.HashPassword("User", password);
-      Console.WriteLine($"Password is {password} and hash {hashedPassword}");
+      user.PlainTextPassword = Console.ReadLine();
+      user.HashedPassword = hasher.HashPassword(user, user.PlainTextPassword);
+
+      Console.WriteLine($"Password is {user.PlainTextPassword} and hash {user.HashedPassword}");
+
       Console.WriteLine("What is your password?");
       var password1 = Console.ReadLine();
-      var isMatch = hasher.VerifyHashedPassword("User", hashedPassword, password1);
+
+      var isMatch = hasher.VerifyHashedPassword(user, user.HashedPassword, password1);
       var result = isMatch.ToString() == "Success" ? "Access granted" : "Access denied";
       Console.WriteLine(result);
     }
+  }
+
+  class UserModel
+  {
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string UserName { get; set; }
+    public string PlainTextPassword { get; set; }
+    public string HashedPassword { get; set; }
   }
 }
